929926e78930b2b109dac9c819826ce920139c6e
Robert Blades <robert.blades1@gmail.com>
2016-04-14 12:23:11 -0400

    Set-up node-notifier in settings.js

--- app/js/settings.js
+++ app/js/settings.js
@@ -4,6 +4,8 @@
 
 const ipcMain = electron.ipcMain;
 const configuration = require('../configuration');
+const notifier = require('../node-notifier');
+const path = require('path');
 
 let closeEl = document.querySelector('.close');
 
@@ -26,6 +28,24 @@ function twoNotify(input) {
   }
   
   if (input.off){
-    
+
   }
 }
+
+notifier.notify({
+  title: 'Reminder!',
+  message: 'Time to do your daily sentence completion exercises',
+  icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+  sound: true, // Only Notification Center or Windows Toasters
+  wait: true // Wait with callback, until user action is taken against notification
+}, function (err, response) {
+  // Response is response from notification
+});
+
+notifier.on('click', function (notifierObject, options) {
+  // Triggers if `wait: true` and user clicks notification
+});
+
+notifier.on('timeout', function (notifierObject, options) {
+  // Triggers if `wait: true` and notification closes
+});
